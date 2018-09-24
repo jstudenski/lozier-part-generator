f324165a95042d1d52f7dcb4a0304fba61aaf750
Jeff Studenski <jstudenski@gmail.com>
2018-09-24 17:29:42 -0500

    fixture css clean up

--- src/components/Fixtures/Fixture.js
+++ src/components/Fixtures/Fixture.js
@@ -3,14 +3,10 @@ import { Draggable } from 'react-beautiful-dnd';
 import DeleteButton from '../DeleteButton';
 
 const getItemStyle = (isDragging, draggableStyle, details) => ({
-  userSelect: 'none',
   width: details.width * 2,
   height: details.height * 2,
-  background: isDragging ? 'salmon' : '#e0e0e0',
-  fontFamily: 'Roboto',
-  textAlign: 'center',
-  position: 'relative',
-  // styles we need to apply on draggables
+  background: isDragging ? '#9e9e9e' : null,
+  // styles neede to apply on draggables
   ...draggableStyle
 });
 
@@ -34,7 +30,7 @@ const Fixture = (props) => {
             details
           )}>
           <DeleteButton click={click} />
-          {details.width} x {details.height}
+          {details.width}x{details.height}
         </div>
       )}
     </Draggable>
